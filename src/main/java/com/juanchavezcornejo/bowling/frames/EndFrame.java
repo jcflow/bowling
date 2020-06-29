93647cd5230e9c4d31761a11dc8f19dd67af4cd6
Juan <im.jhcc@gmail.com>
2020-06-29 09:02:44 -0400

    Implementing End Frame methods.

--- src/main/java/com/juanchavezcornejo/bowling/frames/EndFrame.java
+++ src/main/java/com/juanchavezcornejo/bowling/frames/EndFrame.java
@@ -2,37 +2,96 @@ package com.juanchavezcornejo.bowling.frames;
 
 import com.juanchavezcornejo.bowling.score.Score;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EndFrame implements BowlingFrame {
+    public BowlingFrame previous;
+    protected Score firstRoll;
+    protected Score secondRoll;
+    protected Score thirdRoll;
+
+    public EndFrame() {
+        this.previous = null;
+        this.firstRoll = null;
+        this.secondRoll = null;
+        this.thirdRoll = null;
+    }
+
     @Override
     public int retrieveSum() {
-        return 0;
+        if (this.previous == null || this.firstRoll == null || this.secondRoll == null || this.thirdRoll == null) {
+            throw new RuntimeException();
+        }
+        int sum = 0;
+        if (this.firstRoll == Score.STRIKE) {
+            sum = this.previous.retrieveSum() + 10 + this.retrieveNextScore(1).getValue()
+                    + this.retrieveNextScore(2).getValue();
+        } if (this.secondRoll == Score.STRIKE) {
+            sum = this.previous.retrieveSum() + 10 + this.retrieveNextScore(0).getValue()
+                    + this.retrieveNextScore(2).getValue();
+        } if (this.thirdRoll == Score.STRIKE) {
+            sum = this.previous.retrieveSum() + 10 + this.retrieveNextScore(0).getValue()
+                    + this.retrieveNextScore(1).getValue();
+        } else if (this.firstRoll.getValue() + this.secondRoll.getValue() == 10) {
+            sum = this.previous.retrieveSum() + 10 + this.thirdRoll.getValue();
+        } else if (this.secondRoll.getValue() + this.thirdRoll.getValue() == 10) {
+            sum = this.previous.retrieveSum() + 10 + this.firstRoll.getValue();
+        } else {
+            return this.previous.retrieveSum() + this.firstRoll.getValue() + this.secondRoll.getValue()
+                    + this.thirdRoll.getValue();
+        }
+        return sum;
     }
 
     @Override
     public Score retrieveNextScore(int i) {
-        return null;
+        Score score = null;
+        if (i <= 0) {
+            if (this.secondRoll != null && this.firstRoll != Score.NONE) {
+                return this.firstRoll;
+            }
+            score = this.retrieveNextScore(i + 1);
+        } else if (i == 1 && this.secondRoll != null && this.secondRoll != Score.NONE) {
+            score = this.secondRoll;
+        } else if (i >= 2 && this.thirdRoll != null && this.thirdRoll != Score.NONE) {
+            score = this.thirdRoll;
+        }
+        if (score != null) {
+            return score;
+        }
+        throw new RuntimeException();
     }
 
     @Override
     public void addScore(Score score) {
-
+        if (score == null) {
+            throw new RuntimeException();
+        }
+        if (this.firstRoll == null) {
+            this.firstRoll = score;
+        } else if (this.secondRoll == null) {
+            this.secondRoll = score;
+        } else if (this.thirdRoll == null) {
+            this.thirdRoll = score;
+        } else {
+            throw new RuntimeException();
+        }
     }
 
     @Override
     public void setPrevious(BowlingFrame previous) {
-
+        this.previous = previous;
     }
 
     @Override
     public BowlingFrame getPrevious() {
-        return null;
+        return this.previous;
     }
 
     @Override
     public void setNext(BowlingFrame next) {
-
+        throw new RuntimeException();
     }
 
     @Override
@@ -42,26 +101,47 @@ public class EndFrame implements BowlingFrame {
 
     @Override
     public boolean hasStrike() {
-        return false;
+        return this.firstRoll == Score.STRIKE ||
+                this.secondRoll == Score.STRIKE ||
+                this.thirdRoll == Score.STRIKE;
     }
 
     @Override
     public boolean hasSpare() {
-        return false;
+        return this.firstRoll.getValue() + this.secondRoll.getValue() == 10 ||
+                this.secondRoll.getValue() + this.thirdRoll.getValue() == 10;
     }
 
     @Override
     public boolean canAddScore() {
-        return false;
+        return this.firstRoll == null || this.secondRoll == null || this.thirdRoll == null;
     }
 
     @Override
     public List<Score> retrieveScoreList() {
-        return null;
+        List<Score> scores = new ArrayList<>();
+        if (this.firstRoll != null) {
+            scores.add(this.firstRoll);
+        }
+        if (this.firstRoll != null && this.secondRoll != null) {
+            if (this.firstRoll != Score.STRIKE && this.firstRoll.getValue() + this.secondRoll.getValue() == 10) {
+                scores.add(Score.SPARE);
+            } else {
+                scores.add(this.secondRoll);
+            }
+        }
+        if (this.secondRoll != null && this.thirdRoll != null) {
+            if (this.secondRoll != Score.STRIKE && this.secondRoll.getValue() + this.thirdRoll.getValue() == 10) {
+                scores.add(Score.SPARE);
+            } else {
+                scores.add(this.thirdRoll);
+            }
+        }
+        return scores;
     }
 
     @Override
     public int retrieveSize() {
-        return 0;
+        return 3;
     }
 }
